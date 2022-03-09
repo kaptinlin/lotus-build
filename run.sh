ce1922a01457b440469e230ee9a44ad175d58ab1
kaptinlin <kaptinlin@gmail.com>
2022-03-09 19:01:18 +0800

    update

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # rm -rf lotus
-# git clone https://github.com/filecoin-project/lotus.git
-# cd lotus
-# # # git pull
-# git checkout v1.14.4
+git clone https://github.com/filecoin-project/lotus.git
+cd lotus
+# # git pull
+git checkout v1.14.4
 
-# echo "Building image..."
+echo "Building image..."
 
-# RUSTFLAGS="-C target-cpu=native" FFI_BUILD_FROM_SOURCE=1 make deps
-# cd ..
+RUSTFLAGS="-C target-cpu=native" FFI_BUILD_FROM_SOURCE=1 make deps
+cd ..
 
 docker build -t kaptinlin/lotus:latest --network host .
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
